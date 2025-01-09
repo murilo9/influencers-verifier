f938cd01bafb20c6966bb24a2d17d6c705889cc7
murilo9 <murilohenriquematias@gmail.com>
2025-01-09 16:49:32 -0300

    added production base url

--- packages/app/src/http.ts
+++ packages/app/src/http.ts
@@ -1,3 +1,5 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.PROD
+  ? "https://influencer-checker-api.sitehenger.com"
+  : "http://localhost:3000";
 
 export const makeUrl = (route: string) => BASE_URL + route;
