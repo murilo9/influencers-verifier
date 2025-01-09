a97188182c10d0ad4a762c838b6323698c478909
murilo9 <murilohenriquematias@gmail.com>
2025-01-09 16:33:33 -0300

    api port logging

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -6,6 +6,7 @@ import "@tensorflow/tfjs-node";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
+  console.log("PORT", process.env.PORT);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
